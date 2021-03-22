617d18f2711dea3f1e037aeb0a258ab3c5265337
relund <junk@relund.dk>
2021-03-22 15:13:58 +0000

    Initial commit

---
+++ git_setup.sh
@@ -0,0 +1,5 @@
+git init
+git remote add origin git@github.com:OWNER/REPONAME.git
+git push -u origin master
+git config --global user.email "junk@relund.dk"
+git config --global user.name "relund"
---
+++ setup.R
@@ -0,0 +1,6 @@
+library(usethis)
+use_git_config(user.name = "relund", user.email = "junk@relund.dk")
+use_git()
+
+## create github repository and configure as git remote
+use_github()
